2bd0a55fc6212ec1aec5a5a9916b4a29c9259c4b
CREVTIVE <146997842+CREVTIVE@users.noreply.github.com>
2023-10-05 10:58:38 -0400

    Update sketch_flash.js

--- flash_script/sketch_flash.js
+++ flash_script/sketch_flash.js
@@ -16,6 +16,8 @@ var starterText = "WE'RE\nCREVTIVE";
 var rampCounter = 0;
 
 var thisFont = 0;
+var thisFontAdjust = 1;
+var thisFontAdjustUp = 0;
 
 var flashCount = 13;
 var sceneOn = [];
@@ -48,6 +50,8 @@ function preload(){
   tFont[0] = loadFont("resources/CREVTIVE-Extended-Black.woff");
 
   currentFont = tFont[0];
+  thisFontAdjust = 1;
+  thisFontAdjustUp = 0;
 }
 
 function setup(){
