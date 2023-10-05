1be866cfce37c048d45c45c731df95b7a8911b29
CREVTIVE <146997842+CREVTIVE@users.noreply.github.com>
2023-10-05 11:42:58 -0400

    Update 0_box.js

--- flash_script/0_box.js
+++ flash_script/0_box.js
@@ -121,35 +121,36 @@ class Box {
       this.pgTextSize += 2;
     }
 
-    if(this.pgTextSize * thisFontAdjust > height * 7/8){
-      this.pgTextSize = (height * 7/8)/thisFontAdjust;
+    if(this.pgTextSize * thisFontAdjust > height * 1){
+      this.pgTextSize = (height * 1)/thisFontAdjust;
     }
   }
 
-drawTextures(){
-  textSize(this.pgTextSize);
-  textFont(currentFont);
-  var repeatSize = round(textWidth(this.inp));
+  drawTextures(){
+    textSize(this.pgTextSize);
+    textFont(currentFont);
+    var repeatSize = round(textWidth(this.inp));
+  
+    this.pgA = createGraphics(repeatSize, this.pgTextSize * (thisFontAdjust + 0.1));
+    this.pgA.background(bkgdColor);
+    this.pgA.fill(foreColor);
+    this.pgA.noStroke();
+    this.pgA.textSize(this.pgTextSize);
+    this.pgA.textAlign(CENTER);
+    this.pgA.textFont(currentFont);
+    var thisAdjust = this.pgA.height/2 + this.pgTextSize * thisFontAdjust/2 + this.pgTextSize * thisFontAdjustUp;
+    this.pgA.text(this.inp, this.pgA.width/2, thisAdjust);
 
-  this.pgA = createGraphics(repeatSize, this.pgTextSize * (thisFontAdjust + 0.1));
-  this.pgA.background(bkgdColor);
-  this.pgA.fill(foreColor);
-  this.pgA.noStroke();
-  this.pgA.textSize(this.pgTextSize);
-  this.pgA.textAlign(CENTER, CENTER);
-  this.pgA.textFont(currentFont);
-  this.pgA.text(this.inp, this.pgA.width/2, this.pgA.height/1);
-
-  this.pgB = createGraphics(repeatSize, this.pgTextSize * 0.8);
-  this.pgB.background(foreColor);
-  this.pgB.fill(bkgdColor);
-  this.pgB.noStroke();
-  this.pgB.textSize(this.pgTextSize);
-  this.pgB.textAlign(CENTER, CENTER);
-  this.pgB.textFont(currentFont);
-  this.pgB.text(this.inp, this.pgA.width/2, this.pgB.height/1);
-}
-
+    this.pgB = createGraphics(repeatSize, this.pgTextSize * 0.8);
+    this.pgB.background(foreColor);
+    this.pgB.fill(bkgdColor);
+    this.pgB.noStroke();
+    this.pgB.textSize(this.pgTextSize);
+    this.pgB.textAlign(CENTER);
+    this.pgB.textFont(currentFont);
+    var thisAdjust = this.pgA.height/2 + this.pgTextSize * thisFontAdjust/2 + this.pgTextSize * thisFontAdjustUp;
+    this.pgB.text(this.inp, this.pgA.width/2, thisAdjust);
+  }
 
   removeGraphics(){
     this.pgA.remove();
